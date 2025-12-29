dea0355cc518e7d9b73ee4790dc1455391781a3b
ahoken50 <micross50@gmail.com>
2025-12-29 15:17:48 -0500

    feat: Initialize Firebase and export core services.

--- portail-cce-valdor/src/services/firebase.ts
+++ portail-cce-valdor/src/services/firebase.ts
@@ -3,6 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getFunctions } from 'firebase/functions';
+// @ts-ignore
 import { getVertexAI } from 'firebase/vertexai';
 
 const firebaseConfig = {
